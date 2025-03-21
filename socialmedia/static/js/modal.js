f43ed904d93a8975dde6f72627d6ff2433b43f56
shivampatel91204 <shivampatel91204@gmail.com>
2025-03-20 20:03:39 -0400

    Removed unused variables in modal js file

--- socialmedia/static/js/modal.js
+++ socialmedia/static/js/modal.js
@@ -2,8 +2,6 @@ document.getElementById("createPostForm").addEventListener("submit", function(ev
     event.preventDefault();
 
     var imageInput = document.getElementById("imageUpload");
-    var captionInput = document.getElementById("message-text");
-    var formData = new FormData(this);
 
     if (!imageInput.files.length) {
         alert("Please select a picture before submitting your post.");
