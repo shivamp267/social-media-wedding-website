1aeaf2a9c60919af7f74a7d9c38622e53c2e92ef
shivampatel91204 <shivampatel91204@gmail.com>
2025-03-29 22:39:08 -0400

    switched from sqlite to mysql

--- socialmedia/socialmedia/settings.py
+++ socialmedia/socialmedia/settings.py
@@ -77,10 +77,21 @@ WSGI_APPLICATION = 'socialmedia.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'hiralniharwed',
+        'USER': 'root',
+        'PASSWORD': 'Soccerdestroyer12!',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
