501ed2ccf95e0e1816d3bf07f0dab240dfe25177
shivampatel91204 <shivampatel91204@gmail.com>
2025-04-11 11:54:32 -0400

    removed vercel hosting files

--- socialmedia/socialmedia/settings.py
+++ socialmedia/socialmedia/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-wvu!g!4!97#)x(nu^+isfgo27*ljn$9#8y^+zu^2#50c^h05r9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.vercel.app', 'now.sh', '192.168.1.159', 'localhost']
+ALLOWED_HOSTS = ['192.168.1.159', 'localhost']
 
 TIME_ZONE = 'America/New_York'
 USE_TZ = True 
@@ -124,8 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 
 # Default primary key field type
--- socialmedia/socialmedia/urls.py
+++ socialmedia/socialmedia/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('',include('userauth.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns = urlpatterns+static(settings.MEDIA_URL,
+ document_root=settings.MEDIA_ROOT)
